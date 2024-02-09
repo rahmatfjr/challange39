f1653758a236968f9c97db8cf01c47e302673ac4
Rahmat Fajar <rahmatfajar3218@gmail.com>
2024-02-09 17:54:59 +0700

    redux reat

--- redux/client/src/features/user/UserForm.js
+++ redux/client/src/features/user/UserForm.js
@@ -63,6 +63,7 @@ export default function UserForm(props) {
                 </div>
 
                 <div class="col">
+                    
                     <input
                         type="teks"
                         placeholder="insert number"
